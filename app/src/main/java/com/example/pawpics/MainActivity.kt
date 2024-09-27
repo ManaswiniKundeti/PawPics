7a1f71feaf63859ddc6c2328cfcc4706158e7821
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2024-09-26 20:48:16 -0500

    Compose project setup

--- app/src/main/java/com/example/pawpics/MainActivity.kt
+++ app/src/main/java/com/example/pawpics/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.pawpics
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Text("Hello Compose world!!")
+        }
     }
 }
